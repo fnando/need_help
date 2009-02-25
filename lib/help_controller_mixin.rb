919ded96e494a9f86d0da9084fe2e4a8b2183848
Nando Vieira <fnando.vieira@gmail.com>
2009-02-24 22:39:50 -0300

    AJAX now renders show action without layout

--- lib/help_controller_mixin.rb
+++ lib/help_controller_mixin.rb
@@ -19,8 +19,7 @@ module HelpControllerMixin
     respond_to do |wants|
       wants.html { 
         if request.xhr?
-          render :html => content_tag(:h3, @topic.title) + 
-                          content_tag(:div, @topic.formatted_content, :class => "content")
+          render :layout => false
         else
           redirect_to help_category_path(@topic.category, :anchor => @topic.permalink), :status => :moved_permanently
         end
